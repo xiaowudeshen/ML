2efe8ba01060c3f98f01671ba93ea74ae3a3eed1
zhangqianjin <iamzhangqianjin@163.com>
2020-05-07 16:10:15 +0800

    Update xgboost_example.py

--- gbdt/xgboost_example.py
+++ gbdt/xgboost_example.py
@@ -12,16 +12,16 @@ data, target = breast_cancer['data'], breast_cancer['target']
 X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=42)
 
 
-xgb_model = XGBClassifier()
+xgb_model = XGBClassifier(objective='binary:logistic',booster='gbtree')
 parameters = {'n_estimators':[1,10,20,30,40,50,60],'learning_rate': [0.01, 0.02, 0.03], 'max_depth': [4, 5, 6], 'min_child_weight':[1,2,3,4,5,6]}
 clf = GridSearchCV(xgb_model, parameters, scoring='roc_auc',verbose=1,cv=5)
-clf.fit(data, target)
+clf.fit(X_train, y_train)
 print(clf.best_score_)
 print(clf.best_params_)
 
 xgb_model = XGBClassifier(n_estimators=clf.best_params_['n_estimators'],learning_rate=clf.best_params_['learning_rate'],max_depth=clf.best_params_['max_depth'], min_child_weight=clf.best_params_['min_child_weight'])
 xgb_model.fit(X_train, y_train, early_stopping_rounds=10, eval_metric="auc",eval_set=[(X_test, y_test)])
-preds = clf.predict_proba(X_test)
+preds = xgb_model.predict_proba(X_test)
 print(preds[:,1])
 auc = roc_auc_score(y_test, preds[:,1])
 print(auc)
